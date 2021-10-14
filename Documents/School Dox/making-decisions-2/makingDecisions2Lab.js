5a35202cafbe1ee81d076365826467d13a418d86
beauxdelcambre <beaux@stingphotodesign.com>
2021-10-14 16:23:12 -0500

    second commit

--- Documents/School Dox/making-decisions-2/makingDecisions2Lab.js
+++ Documents/School Dox/making-decisions-2/makingDecisions2Lab.js
@@ -7,7 +7,8 @@ var arr = [10,20,30,40,50,60]
 */
 
 //Code Here
-
+let firstItem = arr[0]
+console.log(firstItem)
 
 ////////// PROBLEM 2 //////////
 /*
@@ -15,7 +16,8 @@ var arr = [10,20,30,40,50,60]
 */
 
 //Code Here
-
+let lastItemRemoved = arr.pop()
+console.log(lastItemRemoved)
 ////////// PROBLEM 3 //////////
 
 // Do not edit the code below.
@@ -27,7 +29,9 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
 */
 
 //Code Here
-
+for (let i = 0; i <= family.length -1; i++){
+  console.log(family[i])
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -42,20 +46,46 @@ let evensArr = []
 */
 
 //Code Here
+for (let i = 0; i <= nums.length; i++){
 
-
+  if ((nums[i] % 2) === 0){
+    evensArr.push(nums[i])
+  
+  }
+  
+}
+console.log(evensArr)
 
 ////////// PROBLEM 5 //////////
 
 // Do not edit the code below.
-var score = 74
+var score = 87
 // Do not edit the code above.
 
 /*
-  Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
+  Determine if the letter grade of the given variable 'score'. 
+  If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', 
+  between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
 */
 
 //Code Here
+if (score >= 90){
+  console.log("A")
+
+}else if(score >= 80){
+  console.log("B")
+
+}else if(score >=70){
+  console.log("C")
+
+}else if(score >= 60){
+  console.log("D")
+
+}else{
+  console.log("F")
+}
+
+
 
 
 ////////// Intermediate Problems //////////
@@ -63,7 +93,7 @@ var score = 74
 ////////// PROBLEM 6 //////////
 
 // Do not edit the code below.
-var myFavoriteNumbers = [4,8,12,16,20,24];
+var myFavoriteNumbers = [4,8,12,16,20,24,33,45];
 // Do not edit the code above.
 
 /*
@@ -71,7 +101,8 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 */
 
 //Code Here
-
+// let someNum = myFavoriteNumbers.splice(4, 2)
+//   console.log(someNum)
 
 ////////// PROBLEM 7 //////////
 
@@ -79,7 +110,16 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
 //Code Here
+if (myFavoriteNumbers.length < 7){
+  console.log("there are not enough elements in this array")
+  
+}else if(myFavoriteNumbers.length > 7){
+  someNum = myFavoriteNumbers[6]
+  console.log(someNum)
 
+}else{
+  console.log('there are exaclty 7 elements in this array')
+}
 
 ////////// PROBLEM 8 //////////
 
@@ -90,7 +130,13 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
 
 //Code Here
+for (let i = 0; i < listOfNumbers.length; i++){
 
+  if (listOfNumbers[i] % 3 === 0){
+
+    console.log(`${listOfNumbers[i]} is divisble by 3`)
+  }
+}
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
@@ -102,7 +148,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+for (let i = letters.length -1; i >= 0; i--){
+  console.log(letters[i])
+  
 
+}
 
 ////////// Advanced Problems //////////
 
@@ -124,31 +174,71 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+switch(letterGrade){
+  case 'A':
+    console.log("The student is doing excellently")
+    break
+  case 'B':
+    console.log("The student is doing well.")
+    break
+  case 'C':
+    console.log("The student is doing alright")
+    break
+  case 'D':
+    console.log("The student is not doing very well")
+    break
+  case 'F':
+    console.log("The student is failing")
+    break
+  default:
+    console.log("Not an eligible grade.")
+    break
+
+}
 
 
 
 ////////// PROBLEM 11 //////////
-/* The famous FizzBuzz, Devmountain style!
-  Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, if the number your for loop is currently on is divisible by 3, console.log 'Dev'. If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'. If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
 
-  Your output should look like:
-  1
-  2
-  Dev
-  4
-  Mountain
-  Dev
-  7
-  8
-  Dev
-  mountain
-  11
-  Dev
-  13
-  14
-  Devmountain
-  16
-  ...
-*/
+// Your output should look like:
+// 1
+// 2
+// Dev
+// 4
+// Mountain
+// Dev
+// 7
+// 8
+// Dev
+// mountain
+// 11
+// Dev
+// 13
+// 14
+// Devmountain
+// 16
+// ...
+// */
 
-//Code Here
+// /* The famous FizzBuzz, Devmountain style!
+//   Create a for loop that iterates from 1 to 100. 
+//   In this for loop, using some conditional logic, 
+//   if the number your for loop is currently on is divisible by 3, console.log 'Dev'.
+//    If the number is divisible by 5, console.log 'mountain'. 
+//    If the number is divisible by 5 & 3, console.log 'Devmountain'. 
+//    If the number is not divisible by 5 or 3, console.log the number itself. 
+//    Hint: Look up the modulo operator.
+
+// //Code Here
+
+for(let i= 1; i <= 100; i++ ){
+  if(i % 3 === 0 && i % 5 === 0){
+    console.log("Devmountain")
+  }else if(i % 5 === 0){
+    console.log("mountain") 
+  }else if(i % 3 === 0){
+    console.log("Dev")
+  }else{
+    console.log(i)
+  }
+}
