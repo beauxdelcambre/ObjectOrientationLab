6000bed1c908ba9b24e9685f9d2d006f0a6fb612
beauxdelcambre <beaux@stingphotodesign.com>
2021-10-17 13:55:32 -0500

    initial commit

---
+++ Documents/DevWork/js-basics-1/sldc.js
@@ -0,0 +1,34 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Analysis of Requirements
+// Design
+// Implementation
+// Maintain
+// Planning
+// Testing & Integration
+
+ Planning
+ Analysis of Reqirements
+ Design
+ Implementation
+ Testing and Integration
+ Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set 
+    the values of each variable to a definition of the step.
+*/
+
+let planning = "determine project goals"
+let analysisofRequirements = "user expectations of the project"
+let design = "how the software will look to the user"
+let implementation = "actually writing the code to the software"
+let testingNintegration = "making sure the software works properly and alignes with the anlysis of requirements"
+let maintain = "making sure the software runs smoothly at all times, updating the software"
